feaa18820b04996ff154acba311448357ca2dd67
Steve Walsh <steven.walsh39@gmail.com>
2017-12-21 14:31:55 +0800

    Added dependency

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
-from src.geothoughts_connection import GeothoughtConnection
+from src.geothoughts import Geothoughts
 
 def handler(event, context):
 
-    geothoughts = GeothoughtConnection('aws_postgres',
-                                'password',
-                                'postgres-instance.c4ogucrzk5k0.ap-northeast-1.rds.amazonaws.com',
-                                '5432',
-                                'postgres')
+    pg_db_url = 'postgresql://%s:%s@%s:%s/%s' % ('aws_postgres',
+         'password',
+         'postgres-instance.c4ogucrzk5k0.ap-northeast-1.rds.amazonaws.com',
+         '5432',
+         'postgres')
+    db_wrapper = DatabaseWrapper(pg_db_url)
+    session = db_wrapper.get_session()
+    geothoughts = Geothoughts(session)
+
     http_method = event['method']
 
     if not http_method:
         return "Error, no http_method available"
 
     if http_method == 'GET':
-        thoughts = geothoughts.get_all()
+        thoughts = geothoughts.get_all(_for_geohash('geohash_1'))
         return {"geothoughts": thoughts}
 
     if http_method == 'POST':
@@ -21,6 +25,3 @@ def handler(event, context):
         coordinates = '1234'
         message = 'this is a test'
         geothoughts.add(geohash, coordinates, message)
-
-if __name__=="__main__":
-  print handler(None, None)
